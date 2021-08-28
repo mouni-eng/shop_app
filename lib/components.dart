4bed06df7fb167a7d7e8461271377ec074ea2bb3
mouni-eng <mounirmohamed372@gmail.com>
2021-08-28 11:13:41 +0200

    home screen, categories screen,  dio helper to get data for home and categories

--- lib/components.dart
+++ lib/components.dart
@@ -1,5 +1,10 @@
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:shop_app/constants.dart';
+import 'package:shop_app/models/categorie_model.dart';
+import 'package:shop_app/models/home_model.dart';
 import 'package:shop_app/models/onboarding_model.dart';
 
 Widget onBoardingBuild(OnBoardingModel model) {
@@ -10,20 +15,23 @@ Widget onBoardingBuild(OnBoardingModel model) {
       SizedBox(
         height: 30.0,
       ),
-      Text("${model.title}", style: TextStyle(
-        fontSize: 18.0,
-      ),),
+      Text(
+        "${model.title}",
+        style: TextStyle(
+          fontSize: 18.0,
+        ),
+      ),
     ],
   );
 }
-
 
 void navigateTo(context, Widget screen) {
   Navigator.push(context, MaterialPageRoute(builder: (context) => screen));
 }
 
 void navigateToAndFinish(context, Widget screen) {
-  Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context) => screen), (route) => false);
+  Navigator.pushAndRemoveUntil(context,
+      MaterialPageRoute(builder: (context) => screen), (route) => false);
 }
 
 Widget defaultFormField({
@@ -56,11 +64,11 @@ Widget defaultFormField({
         ),
         suffixIcon: suffix != null
             ? IconButton(
-          onPressed: suffixPressed,
-          icon: Icon(
-            suffix,
-          ),
-        )
+                onPressed: suffixPressed,
+                icon: Icon(
+                  suffix,
+                ),
+              )
             : null,
         border: OutlineInputBorder(),
       ),
@@ -76,18 +84,18 @@ Widget defaultTextField({
   int? maxLines = 1,
   TextDecoration? decoration,
   TextOverflow? overflow,
-  }) => Text(
-text,
-maxLines: maxLines,
-overflow: overflow?? null,
-textAlign: aligment,
-style: TextStyle(
-fontSize: size,
-fontWeight: fontWeight,
-color: color,
-height: height,
-decoration: decoration?? null,
-));
+}) =>
+    Text(text,
+        maxLines: maxLines,
+        overflow: overflow ?? null,
+        textAlign: aligment,
+        style: TextStyle(
+          fontSize: size,
+          fontWeight: fontWeight,
+          color: color,
+          height: height,
+          decoration: decoration ?? null,
+        ));
 
 Widget defaultButton({
   double width = double.infinity,
@@ -117,34 +125,249 @@ Widget defaultButton({
       ),
     );
 
+
+Widget myDivider() => Padding(
+  padding: const EdgeInsetsDirectional.only(
+    start: 20.0,
+  ),
+  child: Container(
+    width: double.infinity,
+    height: 1.0,
+    color: Colors.grey[300],
+  ),
+);
+
 void showToast({
   required String text,
   required ToastState state,
 }) =>
     Fluttertoast.showToast(
-    msg: text,
-    toastLength: Toast.LENGTH_LONG,
-    gravity: ToastGravity.BOTTOM,
-    timeInSecForIosWeb: 5,
-    backgroundColor: chooseColor(state: state),
-    textColor: Colors.white,
-    fontSize: 16.0);
+        msg: text,
+        toastLength: Toast.LENGTH_LONG,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 5,
+        backgroundColor: chooseColor(state: state),
+        textColor: Colors.white,
+        fontSize: 16.0);
 
-enum ToastState {SUCCESS, WARNING, ERROR}
+enum ToastState { SUCCESS, WARNING, ERROR }
 
 Color chooseColor({required ToastState state}) {
+  Color color;
 
-    Color color;
-
-    switch(state) {
-      case ToastState.SUCCESS:
-        color = Colors.green;
-        break;
-      case ToastState.WARNING:
-        color = Colors.amber;
-        break;
-      case ToastState.ERROR:
-        color = Colors.red;
-        break;
-    }  return color;
+  switch (state) {
+    case ToastState.SUCCESS:
+      color = Colors.green;
+      break;
+    case ToastState.WARNING:
+      color = Colors.amber;
+      break;
+    case ToastState.ERROR:
+      color = Colors.red;
+      break;
   }
+  return color;
+}
+
+Widget builderHomeWidget(HomeModel? model, CategoriesModel? categoriesModel) => SingleChildScrollView(
+  physics: BouncingScrollPhysics(),
+  child: Column(
+    children: [
+      CarouselSlider(
+          items: model!.data!.bannerList
+              .map(
+                (e) => Image(
+              image: NetworkImage(e.image.toString()),
+              fit: BoxFit.cover,
+              width: double.infinity,
+            ),
+          )
+              .toList(),
+          options: CarouselOptions(
+            height: 200,
+            viewportFraction: 1.0,
+            enlargeCenterPage: false,
+            initialPage: 0,
+            enableInfiniteScroll: true,
+            reverse: false,
+            autoPlay: true,
+            autoPlayInterval: Duration(seconds: 3),
+            autoPlayAnimationDuration: Duration(seconds: 1),
+            autoPlayCurve: Curves.fastOutSlowIn,
+            scrollDirection: Axis.horizontal,
+          )),
+      SizedBox(
+        height: 14.0,
+      ),
+      Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Categories',
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+            SizedBox(
+              height: 25.0,
+            ),
+            Container(
+              height: 100.0,
+              child: ListView.separated(
+                  physics: BouncingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) => buildCategoriesWidget(categoriesModel!.data!.catData[index]),
+                  separatorBuilder: (context, index) => SizedBox(
+                    width: 15.0,
+                  ),
+                  itemCount: categoriesModel!.data!.catData.length),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              'New Products',
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+            SizedBox(
+              height: 25.0,
+            ),
+            Container(
+              color: Colors.grey[300],
+              child: GridView.count(
+                crossAxisCount: 2,
+                shrinkWrap: true,
+                mainAxisSpacing: 1.0,
+                crossAxisSpacing: 1.0,
+                physics: NeverScrollableScrollPhysics(),
+                childAspectRatio: 1 / 1.76,
+                children: List.generate(
+                    model.data!.productsList.length,
+                        (index) =>
+                        buildProductsWidget(model.data!.productsList[index])),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ],
+  ),
+);
+
+Widget buildProductsWidget(ProductsModel model) => Container(
+  color: Colors.white,
+  child: Column(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Stack(alignment: AlignmentDirectional.bottomStart, children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image(
+            image: NetworkImage(model.image.toString()),
+            height: 180,
+            fit: BoxFit.fill,
+          ),
+        ),
+        if (model.discount != 0)
+          Container(
+            color: Colors.red,
+            padding: EdgeInsets.all(5.0),
+            child: defaultTextField(
+                size: 8.0, text: 'DISCOUNT', color: Colors.white),
+          )
+      ]),
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            defaultTextField(
+                size: 12.0,
+                text: model.name.toString(),
+                color: Colors.black,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+                height: 1.3),
+            Row(
+              children: [
+                defaultTextField(
+                    size: 10.0,
+                    text: '${model.price.toString()} LE',
+                    color: kPrimaryColor),
+                SizedBox(
+                  width: 8,
+                ),
+                if (model.discount != 0)
+                  defaultTextField(
+                    size: 10.0,
+                    text: model.oldPrice.toString(),
+                    color: Colors.grey,
+                    decoration: TextDecoration.lineThrough,
+                  ),
+                Spacer(),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.favorite_border,
+                    size: 14.0,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      )
+    ],
+  ),
+);
+
+Widget buildCategoriesWidget(CatDataModel model) => Stack(
+    alignment: Alignment.bottomLeft,
+    children: [
+      Image(
+        image: NetworkImage(
+            model.image.toString()),
+        height: 100.0,
+        width: 100.0,
+        fit: BoxFit.fill,
+      ),
+      Container(
+        padding: const EdgeInsets.all(4.0),
+        width: 100.0,
+        color: Colors.black.withOpacity(0.8),
+        child: defaultTextField(size: 10.0, text: model.name.toString(), color: Colors.white, overflow: TextOverflow.ellipsis, aligment: TextAlign.center, maxLines: 1),
+      )
+    ]);
+
+Widget buildCategoriesScreen(CatDataModel model) => Column(
+  children: [
+    SizedBox(
+      height: 15.0,
+    ),
+    Container(
+      margin: const EdgeInsets.all(18.0),
+      child: ListTile(
+        leading: Image(
+          image: NetworkImage(
+              model.image.toString()),
+        ),
+        title: defaultTextField(
+            size: 18.0, text: model.name.toString(), color: Colors.black),
+        trailing: IconButton(
+          icon: Icon(
+            Icons.arrow_forward_ios,
+            color: Colors.black,
+          ),
+          onPressed: () {},
+        ),
+      ),
+    ),
+  ],
+);
